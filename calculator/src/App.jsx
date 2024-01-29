c7c96677bc3140cc663d929367ff1efd6378d4be
karishmaaaaa <karishma.ss@kalvium.community>
2024-01-29 10:13:13 +0530

    haan

---
+++ calculator/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DisplayCalculator from "./Component/DisplayCalculator";
+
+function App(){
+  return (
+    <div>
+      <DisplayCalculator/>
+    </div>
+  )
+}
+
+export default App
---
+++ calculator/src/Component/DisplayCalculator.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DisplayScreen from './DisplayScreen.jsx'
+
+function DisplayCalculator() {
+  return(
+    <>
+        <DisplayScreen/>
+    </>
+  )
+}
+
+export default DisplayCalculator;
